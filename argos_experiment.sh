24ac546a35a73f07509f9adc5a9010f41c8f28a4
arsalanakhter <aakhter@wpi.edu>
2023-01-31 16:02:16 -0500

    Bug removed

--- argos_experiment.sh
+++ argos_experiment.sh
@@ -1,4 +1,4 @@
 python3 src/ARGoSConfigurator.py
-cp -f src/kheperaiv_5_tiled.argos ../collective_perception/collective_perception_dynamic/build/src/run_dynamic_simulations -c argos/kheperaiv_5_tiled.argos
+cp -f src/kheperaiv_5_tiled.argos ../collective_perception/collective_perception_dynamic/build/src/run_dynamic_simulations
 cd ../collective_perception/collective_perception_dynamic/build/src
 ./run_dynamic_simulations -c argos/kheperaiv_5_tiled.argos
