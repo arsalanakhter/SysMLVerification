b2ac23e7ae16d1deb1d800eea2c37ba5eb93ea11
arsalanakhter <aakhter@wpi.edu>
2023-02-02 18:07:49 -0500

    Updated function for SysML Hazards Extraction
    The function can now read hazards, associated parents, and tagged values

--- src/SysMLHazardsExtractor.py
+++ src/SysMLHazardsExtractor.py
@@ -7,11 +7,39 @@ def extract_hazards_from_sysml():
     tree = etree.parse(file_name, parser)
     root = tree.getroot()
     for element in root.iter(tag='Stereotype'):
-        if element.attrib['Name'] == 'Hazards':
+        if element.attrib['Name'] == 'Hazard':
             # print(element.attrib)
             desired_parent = element.xpath('../..')
             print(desired_parent[0].attrib['Name'])
+            desired_parent = element.xpath('../../../..')
+            print(desired_parent[0].attrib['Name'])
+
+
+def extract_hazards_from_sysml_2():
+    file_name = '../project (2).xml'
+    parser = etree.XMLParser(remove_blank_text=True)
+    tree = etree.parse(file_name, parser)
+    root = tree.getroot()
+    for element in root.iter(tag='Stereotype'):
+        if element.attrib['Name'] == 'Hazard':
+            # print(element.attrib)
+            desired_parent = element.xpath('../..')
+            print(desired_parent[0].attrib['Name'])
+            desired_parent2 = element.xpath('../../../..')
+            if desired_parent2:
+                print(desired_parent2[0].attrib['Name'])
+            desired_parent = element.xpath(
+                '../../TaggedValues/TaggedValueContainer'+
+                '/ModelChildren')
+            for elem in desired_parent[0].iter():
+                if elem.attrib:
+                    print(elem.attrib['Name'])
+                    if 'Value' in elem.attrib.values():
+                        print(elem.attrib['Value'])
+                    else:
+                        print('No Attrib Value')
+
 
 
 if __name__ == '__main__':
-    extract_hazards_from_sysml()
+    extract_hazards_from_sysml_2()
